0a4cd21d8d0f09a7569bc73332b25c7818875c31
Jun Shern Lim <lim321@purdue.edu>
2021-10-23 23:22:30 -0400

    wav file downloads automatically

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
-navigator.mediaDevices.getUserMedia({audio:true})
-.then(stream => {handlerFunction(stream)})
+navigator.mediaDevices.getUserMedia({ audio: true })
+  .then(stream => { handlerFunction(stream) })
 
 
-      function handlerFunction(stream) {
-      rec = new MediaRecorder(stream);
-      rec.ondataavailable = e => {
-        audioChunks.push(e.data);
-        if (rec.state == "inactive"){
-          let blob = new Blob(audioChunks,{type:'audio/mpeg-3'});
-          recordedAudio.src = URL.createObjectURL(blob);
-          recordedAudio.controls=true;
-          recordedAudio.autoplay=true;
-          sendData(blob)
-        }
-      }
+function handlerFunction(stream) {
+  rec = new MediaRecorder(stream);
+  rec.ondataavailable = e => {
+    audioChunks.push(e.data);
+    if (rec.state == "inactive") {
+      let blob = new Blob(audioChunks, { type: 'audio/wav' });
+      recordedAudio.src = URL.createObjectURL(blob);
+      recordedAudio.controls = true;
+      recordedAudio.autoplay = true;
+      download.href = recordedAudio.src;
+      download.download = 'wav';
+      download.innerHTML = 'download';
+      sendData(blob)
     }
-          function sendData(data) {}
+  }
+}
+function sendData(data) { }
 
-  record.onclick = e => {
-    console.log('I was clicked')
-    record.disabled = true;
-    record.style.backgroundColor = "blue"
-    stopRecord.disabled=false;
-    audioChunks = [];
-    rec.start();
-  }
-  stopRecord.onclick = e => {
-    console.log("I was clicked")
-    record.disabled = false;
-    stop.disabled=true;
-    record.style.backgroundColor = "red"
-    rec.stop();
-  }
+recordedAudio.onloadeddata = e => {
+  document.getElementById("download").click();
+}
+
+record.onclick = e => {
+  console.log('I was clicked')
+  record.disabled = true;
+  record.style.backgroundColor = "blue"
+  stopRecord.disabled = false;
+  audioChunks = [];
+  rec.start();
+}
+stopRecord.onclick = e => {
+  console.log("I was clicked")
+  record.disabled = false;
+  stop.disabled = true;
+  record.style.backgroundColor = "red"
+  rec.stop();
+}
